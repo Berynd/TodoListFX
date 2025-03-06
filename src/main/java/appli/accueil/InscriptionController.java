66b194d3fe65079539951578815a561d35638167
Mateo LAPETRE <m.lapetre-sarthou@lprs.fr>
2025-03-06 16:49:21 +0100

    feat: etape 6 7

--- src/main/java/appli/accueil/InscriptionController.java
+++ src/main/java/appli/accueil/InscriptionController.java
@@ -1,4 +1,37 @@
 package appli.accueil;
 
+import appli.StartApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
 public class InscriptionController {
+
+    @FXML
+    private TextField confirmation;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField mdp;
+
+    @FXML
+    private TextField nom;
+
+    @FXML
+    private TextField prenom;
+
+    @FXML
+    void onInscription(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRetour(ActionEvent event) {
+
+    }
+
 }
+
