070e55bebd7a0c047f2074613fced646f57d9de6
Mateo LAPETRE <m.lapetre-sarthou@lprs.fr>
2025-03-18 09:25:27 +0100

    feat: ajout des model et repo des table restante dans la bdd

---
+++ src/main/java/model/Liste.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Liste {
+
+    private int id_liste;
+
+    private String nom;
+
+    public Liste(int id_liste, String nom) {
+        this.id_liste = id_liste;
+        this.nom = nom;
+    }
+
+    public Liste(String nom) {
+        this.nom = nom;
+    }
+
+    public int getId_liste() {
+        return id_liste;
+    }
+
+    public void setId_liste(int id_liste) {
+        this.id_liste = id_liste;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
+
---
+++ src/main/java/model/Type.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Type {
+    private int id_type;
+    private String nom;
+    private String codeCouleur;
+
+    public Type(int id_type, String nom, String codeCouleur) {
+        this.id_type = id_type;
+        this.nom = nom;
+        this.codeCouleur = codeCouleur;
+    }
+
+    public Type(String codeCouleur, String nom) {
+        this.codeCouleur = codeCouleur;
+        this.nom = nom;
+    }
+
+    public int getId_type() {
+        return id_type;
+    }
+
+    public void setId_type(int id_type) {
+        this.id_type = id_type;
+    }
+
+    public String getCodeCouleur() {
+        return codeCouleur;
+    }
+
+    public void setCodeCouleur(String codeCouleur) {
+        this.codeCouleur = codeCouleur;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
